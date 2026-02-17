c71c0b749822c4a5ba2f3a1e6b7b0d918156c979
Enoch Abassey <enochabassey@gmail.com>
2026-02-17 08:24:02 +0000

    update

--- smql-engine/target/debug/deps/smql-417c9895592d8b15.d
+++
@@ -1,5 +0,0 @@
-/Users/macm1/new-y-combinator/state-db/smql-engine/target/debug/deps/smql-417c9895592d8b15.d: crates/smql-cli/src/main.rs
-
-/Users/macm1/new-y-combinator/state-db/smql-engine/target/debug/deps/smql-417c9895592d8b15: crates/smql-cli/src/main.rs
-
-crates/smql-cli/src/main.rs:
--- smql-engine/target/debug/deps/smql-e4f91f404b4c8ab1.d
+++
@@ -1,5 +0,0 @@
-/Users/macm1/new-y-combinator/state-db/smql-engine/target/debug/deps/smql-e4f91f404b4c8ab1.d: crates/smql-cli/src/main.rs
-
-/Users/macm1/new-y-combinator/state-db/smql-engine/target/debug/deps/smql-e4f91f404b4c8ab1: crates/smql-cli/src/main.rs
-
-crates/smql-cli/src/main.rs:
