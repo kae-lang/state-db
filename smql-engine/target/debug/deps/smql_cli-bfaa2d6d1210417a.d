32df35817d1ea8ba4eb1295186ec4fd7a1db194f
Enoch Abassey <enochabassey@gmail.com>
2026-02-18 08:38:19 +0000

    clean 2

--- smql-engine/target/debug/deps/smql_cli-bfaa2d6d1210417a.d
+++
@@ -1,8 +0,0 @@
-/Users/macm1/new-y-combinator/state-db/smql-engine/target/debug/deps/smql_cli-bfaa2d6d1210417a.d: crates/smql-cli/src/lib.rs crates/smql-cli/src/repl.rs
-
-/Users/macm1/new-y-combinator/state-db/smql-engine/target/debug/deps/libsmql_cli-bfaa2d6d1210417a.rlib: crates/smql-cli/src/lib.rs crates/smql-cli/src/repl.rs
-
-/Users/macm1/new-y-combinator/state-db/smql-engine/target/debug/deps/libsmql_cli-bfaa2d6d1210417a.rmeta: crates/smql-cli/src/lib.rs crates/smql-cli/src/repl.rs
-
-crates/smql-cli/src/lib.rs:
-crates/smql-cli/src/repl.rs:
